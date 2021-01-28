50affcfa57a3b4b96259f75c3528b1362c767fd7
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-01-28 16:03:37 +0300

    добавлены слои сверточных нейронных сетей

--- capsnet/capsule_layers.py
+++ capsnet/capsule_layers.py
@@ -1,84 +1,408 @@
-import keras.backend as backend
+import keras.backend as K
 import tensorflow as tf
 from keras import initializers, layers
 from keras.utils.conv_utils import conv_output_length, deconv_length
 import numpy as np
 
 
-# TODO: Utils
 class UtilsCapsNet:
     @staticmethod
-    def update_routing(votes, biases, logit_shape, num_dims, input_dim, output_dim, num_routing):
-        pass
+    def update_locally_constrained_routing(votes, biases, logit_shape, num_dims, input_dim, output_dim,
+                       num_routing):
+        if num_dims == 6:
+            votes_t_shape = [5, 0, 1, 2, 3, 4]
+            r_t_shape = [1, 2, 3, 4, 5, 0]
+        elif num_dims == 4:
+            votes_t_shape = [3, 0, 1, 2]
+            r_t_shape = [1, 2, 3, 0]
+        else:
+            raise NotImplementedError('Not implemented')
+
+        votes_trans = tf.transpose(votes, votes_t_shape)
+        _, _, _, height, width, caps = votes_trans.get_shape()
+
+        def _body(i, logits, activations):
+            """Routing while loop."""
+            # route: [batch, input_dim, output_dim, ...]
+            route = tf.nn.softmax(logits, dim=-1)
+            preactivate_unrolled = route * votes_trans
+            preact_trans = tf.transpose(preactivate_unrolled, r_t_shape)
+            preactivate = tf.reduce_sum(preact_trans, axis=1) + biases
+            activation = UtilsCapsNet.squash(preactivate)
+            activations = activations.write(i, activation)
+            act_3d = K.expand_dims(activation, 1)
+            tile_shape = np.ones(num_dims, dtype=np.int32).tolist()
+            tile_shape[1] = input_dim
+            act_replicated = tf.tile(act_3d, tile_shape)
+            distances = tf.reduce_sum(votes * act_replicated, axis=-1)
+            logits += distances
+            return (i + 1, logits, activations)
+
+        activations = tf.TensorArray(
+            dtype=tf.float32, size=num_routing, clear_after_read=False)
+        logits = tf.fill(logit_shape, 0.0)
+
+        i = tf.constant(0, dtype=tf.int32)
+        _, logits, activations = tf.while_loop(
+            lambda i, logits, activations: i < num_routing,
+            _body,
+            loop_vars=[i, logits, activations],
+            swap_memory=True)
+
+        return K.cast(activations.read(num_routing - 1), dtype='float32')
 
     @staticmethod
-    def squash(input_tensor):
-        pass
+    def squash(input_tensor, axis=-1):
+        norm = tf.norm(input_tensor, axis, keepdims=True)
+        norm_squared = norm * norm
+        return (input_tensor / norm) * (norm_squared / (1 + norm_squared))
 
 
-# TODO: Length layer
 class Length(layers.Layer):
-    def __init__(self, num_classes, seg=True, **kwargs):
-        pass
+    def __init__(self, num_classes, seg=False, **kwargs):
+        super(Length, self).__init__(**kwargs)
+        if num_classes == 2:
+            self.num_classes = 1
+        else:
+            self.num_classes = num_classes
+        self.seg = seg
 
     def call(self, inputs, **kwargs):
-        pass
+        if inputs.get_shape().ndims == 5:
+            assert inputs.get_shape()[-2].value == 1, 'Error: Must have num_capsules = 1 going into Length'
+            inputs = K.squeeze(inputs, axis=-2)
+        return K.expand_dims(tf.norm(inputs, axis=-1), axis=-1)
 
     def compute_output_shape(self, input_shape):
-        pass
+        if len(input_shape) == 5:
+            input_shape = input_shape[0:-2] + input_shape[-1:]
+        if self.seg:
+            return input_shape[:-1] + (self.num_classes,)
+        else:
+            return input_shape[:-1]
 
     def get_config(self):
-        pass
+        config = {'num_classes': self.num_classes, 'seg': self.seg}
+        base_config = super(Length, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
-# TODO: Mask layer
 class Mask(layers.Layer):
     def __init__(self, resize_masks=False, **kwargs):
-        pass
+        super(Mask, self).__init__(**kwargs)
+        self.resize_masks = resize_masks
 
     def call(self, inputs, **kwargs):
-        pass
+        if type(inputs) is list:
+            assert len(inputs) == 2
+            input, mask = inputs
+            _, hei, wid, _, _ = input.get_shape()
+            if self.resize_masks:
+                mask = tf.image.resize_bicubic(mask, (hei.value, wid.value))
+            mask = K.expand_dims(mask, -1)
+            if input.get_shape().ndims == 3:
+                masked = K.batch_flatten(mask * input)
+            else:
+                masked = mask * input
+
+        else:
+            if inputs.get_shape().ndims == 3:
+                x = K.sqrt(K.sum(K.square(inputs), -1))
+                mask = K.one_hot(indices=K.argmax(x, 1), num_classes=x.get_shape().as_list()[1])
+                masked = K.batch_flatten(K.expand_dims(mask, -1) * inputs)
+            else:
+                masked = inputs
+
+        return masked
 
     def compute_output_shape(self, input_shape):
-        pass
+        if type(input_shape[0]) is tuple:  # true label provided
+            if len(input_shape[0]) == 3:
+                return tuple([None, input_shape[0][1] * input_shape[0][2]])
+            else:
+                return input_shape[0]
+        else:  # no true label provided
+            if len(input_shape) == 3:
+                return tuple([None, input_shape[1] * input_shape[2]])
+            else:
+                return input_shape
 
     def get_config(self):
-        pass
+        config = {'resize_masks': self.resize_masks}
+        base_config = super(Mask, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
-# TODO: ConvCapsule layer
+class CapsuleLayer(layers.Layer):
+    def __init__(self, num_capsule, dim_capsule, routings=3,
+                 kernel_initializer='glorot_uniform',
+                 **kwargs):
+        super(CapsuleLayer, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.kernel_initializer = initializers.get(kernel_initializer)
+
+    def build(self, input_shape):
+        assert len(input_shape) >= 3, "The input Tensor should have shape=[None, input_num_capsule, input_dim_capsule]"
+        self.input_num_capsule = input_shape[1]
+        self.input_dim_capsule = input_shape[2]
+
+        self.W = self.add_weight(shape=[self.num_capsule, self.input_num_capsule,
+                                        self.dim_capsule, self.input_dim_capsule],
+                                 initializer=self.kernel_initializer,
+                                 name='W')
+
+        self.built = True
+
+    def call(self, inputs, training=None):
+        inputs_expand = K.expand_dims(inputs, 1)
+        inputs_tiled = K.tile(inputs_expand, [1, self.num_capsule, 1, 1])
+        inputs_hat = K.map_fn(lambda x: K.batch_dot(x, self.W, [2, 3]), elems=inputs_tiled)
+        b = tf.zeros(shape=[K.shape(inputs_hat)[0], self.num_capsule, self.input_num_capsule])
+
+        assert self.routings > 0, 'The routings should be > 0.'
+        for i in range(self.routings):
+            c = tf.nn.softmax(b, dim=1)
+            outputs = UtilsCapsNet.squash(K.batch_dot(c, inputs_hat, [2, 2]))  # [None, 10, 16]
+            if i < self.routings - 1:
+                b += K.batch_dot(outputs, inputs_hat, [2, 3])
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return tuple([None, self.num_capsule, self.dim_capsule])
+
+    def get_config(self):
+        config = {
+            'num_capsule': self.num_capsule,
+            'dim_capsule': self.dim_capsule,
+            'routings': self.routings
+        }
+        base_config = super(CapsuleLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+def PrimaryCaps(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
+    output = layers.Conv2D(filters=dim_capsule * n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
+                           name='primarycaps_conv2d')(inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_capsule], name='primarycaps_reshape')(output)
+    return layers.Lambda(UtilsCapsNet.squash, name='primarycaps_squash')(outputs)
+
+
 class ConvCapsuleLayer(layers.Layer):
     def __init__(self, kernel_size, num_capsule, num_atoms, strides=1, padding='same', routings=3,
                  kernel_initializer='he_normal', **kwargs):
-        pass
+        super(ConvCapsuleLayer, self).__init__(**kwargs)
+        self.kernel_size = kernel_size
+        self.num_capsule = num_capsule
+        self.num_atoms = num_atoms
+        self.strides = strides
+        self.padding = padding
+        self.routings = routings
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.input_height = None
+        self.input_width = None
+        self.input_num_capsule = None
+        self.input_num_atoms = None
 
     def build(self, input_shape):
-        pass
+        assert len(input_shape) == 5, "The input Tensor should have shape=[None, input_height, input_width," \
+                                      " input_num_capsule, input_num_atoms]"
+        self.input_height = input_shape[1]
+        self.input_width = input_shape[2]
+        self.input_num_capsule = input_shape[3]
+        self.input_num_atoms = input_shape[4]
+
+        # Transform matrix
+        self.W = self.add_weight(shape=[self.kernel_size, self.kernel_size,
+                                 self.input_num_atoms, self.num_capsule * self.num_atoms],
+                                 initializer=self.kernel_initializer,
+                                 name='W')
+
+        self.b = self.add_weight(shape=[1, 1, self.num_capsule, self.num_atoms],
+                                 initializer=initializers.constant(0.1),
+                                 name='b')
+
+        self.built = True
 
     def call(self, input_tensor, training=None):
-        pass
+
+        input_transposed = tf.transpose(input_tensor, [3, 0, 1, 2, 4])
+        input_shape = K.shape(input_transposed)
+        input_tensor_reshaped = K.reshape(input_transposed, [
+            input_shape[0] * input_shape[1], self.input_height, self.input_width, self.input_num_atoms])
+        input_tensor_reshaped.set_shape((None, self.input_height, self.input_width, self.input_num_atoms))
+
+        conv = K.conv2d(input_tensor_reshaped, self.W, (self.strides, self.strides),
+                        padding=self.padding, data_format='channels_last')
+
+        votes_shape = K.shape(conv)
+        _, conv_height, conv_width, _ = conv.get_shape()
+
+        votes = K.reshape(conv, [input_shape[1], input_shape[0], votes_shape[1], votes_shape[2],
+                                 self.num_capsule, self.num_atoms])
+        votes.set_shape((None, self.input_num_capsule, conv_height.value, conv_width.value,
+                         self.num_capsule, self.num_atoms))
+
+        logit_shape = K.stack([
+            input_shape[1], input_shape[0], votes_shape[1], votes_shape[2], self.num_capsule])
+        biases_replicated = K.tile(self.b, [conv_height.value, conv_width.value, 1, 1])
+
+        activations = UtilsCapsNet.update_locally_constrained_routing(
+            votes=votes,
+            biases=biases_replicated,
+            logit_shape=logit_shape,
+            num_dims=6,
+            input_dim=self.input_num_capsule,
+            output_dim=self.num_capsule,
+            num_routing=self.routings)
+
+        return activations
 
     def compute_output_shape(self, input_shape):
-        pass
+        space = input_shape[1:-2]
+        new_space = []
+        for i in range(len(space)):
+            new_dim = conv_output_length(
+                space[i],
+                self.kernel_size,
+                padding=self.padding,
+                stride=self.strides,
+                dilation=1)
+            new_space.append(new_dim)
+
+        return (input_shape[0],) + tuple(new_space) + (self.num_capsule, self.num_atoms)
 
     def get_config(self):
-        pass
+        config = {
+            'kernel_size': self.kernel_size,
+            'num_capsule': self.num_capsule,
+            'num_atoms': self.num_atoms,
+            'strides': self.strides,
+            'padding': self.padding,
+            'routings': self.routings,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer)
+        }
+        base_config = super(ConvCapsuleLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
-# TODO: DeconvCapsule layer
 class DeconvCapsuleLayer(layers.Layer):
-    def __init__(self, kernel_size, num_capsule, num_atoms, scalling=2, upsamp_type='deconv', padding='same', routings=3,
+    def __init__(self, kernel_size, num_capsule, num_atoms, scaling=2, upsamp_type='deconv', padding='same', routings=3,
                  kernel_initializer='he_normal', **kwargs):
-        pass
+        super(DeconvCapsuleLayer, self).__init__(**kwargs)
+        self.kernel_size = kernel_size
+        self.num_capsule = num_capsule
+        self.num_atoms = num_atoms
+        self.scaling = scaling
+        self.upsamp_type = upsamp_type
+        self.padding = padding
+        self.routings = routings
+        self.kernel_initializer = initializers.get(kernel_initializer)
 
     def build(self, input_shape):
-        pass
+        assert len(input_shape) == 5, "The input Tensor should have shape=[None, input_height, input_width," \
+                                      " input_num_capsule, input_num_atoms]"
+        self.input_height = input_shape[1]
+        self.input_width = input_shape[2]
+        self.input_num_capsule = input_shape[3]
+        self.input_num_atoms = input_shape[4]
+
+        # Transform matrix
+        if self.upsamp_type == 'subpix':
+            self.W = self.add_weight(shape=[self.kernel_size, self.kernel_size,
+                                            self.input_num_atoms,
+                                            self.num_capsule * self.num_atoms * self.scaling * self.scaling],
+                                     initializer=self.kernel_initializer,
+                                     name='W')
+        elif self.upsamp_type == 'resize':
+            self.W = self.add_weight(shape=[self.kernel_size, self.kernel_size,
+                                     self.input_num_atoms, self.num_capsule * self.num_atoms],
+                                     initializer=self.kernel_initializer, name='W')
+        elif self.upsamp_type == 'deconv':
+            self.W = self.add_weight(shape=[self.kernel_size, self.kernel_size,
+                                            self.num_capsule * self.num_atoms, self.input_num_atoms],
+                                     initializer=self.kernel_initializer, name='W')
+        else:
+            raise NotImplementedError('Upsampling must be one of: "deconv", "resize", or "subpix"')
+
+        self.b = self.add_weight(shape=[1, 1, self.num_capsule, self.num_atoms],
+                                 initializer=initializers.constant(0.1),
+                                 name='b')
+
+        self.built = True
 
     def call(self, input_tensor, training=None):
-        pass
+        input_transposed = tf.transpose(input_tensor, [3, 0, 1, 2, 4])
+        input_shape = K.shape(input_transposed)
+        input_tensor_reshaped = K.reshape(input_transposed, [
+            input_shape[1] * input_shape[0], self.input_height, self.input_width, self.input_num_atoms])
+        input_tensor_reshaped.set_shape((None, self.input_height, self.input_width, self.input_num_atoms))
+
+
+        if self.upsamp_type == 'resize':
+            upsamp = K.resize_images(input_tensor_reshaped, self.scaling, self.scaling, 'channels_last')
+            outputs = K.conv2d(upsamp, kernel=self.W, strides=(1, 1), padding=self.padding, data_format='channels_last')
+        elif self.upsamp_type == 'subpix':
+            conv = K.conv2d(input_tensor_reshaped, kernel=self.W, strides=(1, 1), padding='same',
+                            data_format='channels_last')
+            outputs = tf.depth_to_space(conv, self.scaling)
+        else:
+            batch_size = input_shape[1] * input_shape[0]
+
+            # Infer the dynamic output shape:
+            out_height = deconv_length(self.input_height, self.scaling, self.kernel_size, self.padding)
+            out_width = deconv_length(self.input_width, self.scaling, self.kernel_size, self.padding)
+            output_shape = (batch_size, out_height, out_width, self.num_capsule * self.num_atoms)
+
+            outputs = K.conv2d_transpose(input_tensor_reshaped, self.W, output_shape, (self.scaling, self.scaling),
+                                     padding=self.padding, data_format='channels_last')
+
+        votes_shape = K.shape(outputs)
+        _, conv_height, conv_width, _ = outputs.get_shape()
+
+        votes = K.reshape(outputs, [input_shape[1], input_shape[0], votes_shape[1], votes_shape[2],
+                                 self.num_capsule, self.num_atoms])
+        votes.set_shape((None, self.input_num_capsule, conv_height.value, conv_width.value,
+                         self.num_capsule, self.num_atoms))
+
+        logit_shape = K.stack([
+            input_shape[1], input_shape[0], votes_shape[1], votes_shape[2], self.num_capsule])
+        biases_replicated = K.tile(self.b, [votes_shape[1], votes_shape[2], 1, 1])
+
+        activations = UtilsCapsNet.update_locally_constrained_routing(
+            votes=votes,
+            biases=biases_replicated,
+            logit_shape=logit_shape,
+            num_dims=6,
+            input_dim=self.input_num_capsule,
+            output_dim=self.num_capsule,
+            num_routing=self.routings)
+
+        return activations
 
     def compute_output_shape(self, input_shape):
-        pass
+        output_shape = list(input_shape)
+
+        output_shape[1] = deconv_length(output_shape[1], self.scaling, self.kernel_size, self.padding)
+        output_shape[2] = deconv_length(output_shape[2], self.scaling, self.kernel_size, self.padding)
+        output_shape[3] = self.num_capsule
+        output_shape[4] = self.num_atoms
+
+        return tuple(output_shape)
 
     def get_config(self):
-        pass
+        config = {
+            'kernel_size': self.kernel_size,
+            'num_capsule': self.num_capsule,
+            'num_atoms': self.num_atoms,
+            'scaling': self.scaling,
+            'padding': self.padding,
+            'upsamp_type': self.upsamp_type,
+            'routings': self.routings,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer)
+        }
+        base_config = super(DeconvCapsuleLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
