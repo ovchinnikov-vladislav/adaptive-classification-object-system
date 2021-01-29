5f3fe7428407b0d72c08cca41a1f680b2e1f2bb3
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-01-29 20:29:33 +0300

    fix standard capsnet

--- main.py
+++ main.py
@@ -1,16 +1,213 @@
-# This is a sample Python script.
+import csv
+import math
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tensorflow.keras import initializers, layers
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models, optimizers
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from PIL import Image
+from capsnet import capsnets
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def plot_log(filename, show=True):
+
+    data = pandas.read_csv(filename)
+
+    fig = plt.figure(figsize=(4,6))
+    fig.subplots_adjust(top=0.95, bottom=0.05, right=0.95)
+    fig.add_subplot(211)
+    for key in data.keys():
+        if key.find('loss') >= 0 and not key.find('val') >= 0:  # training loss
+            plt.plot(data['epoch'].values, data[key].values, label=key)
+    plt.legend()
+    plt.title('Training loss')
+
+    fig.add_subplot(212)
+    for key in data.keys():
+        if key.find('acc') >= 0:  # acc
+            plt.plot(data['epoch'].values, data[key].values, label=key)
+    plt.legend()
+    plt.title('Training and validation accuracy')
+
+    # fig.savefig('result/log.png')
+    if show:
+        plt.show()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def combine_images(generated_images, height=None, width=None):
+    num = generated_images.shape[0]
+    if width is None and height is None:
+        width = int(math.sqrt(num))
+        height = int(math.ceil(float(num)/width))
+    elif width is not None and height is None:  # height not given
+        height = int(math.ceil(float(num)/width))
+    elif height is not None and width is None:  # width not given
+        width = int(math.ceil(float(num)/height))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
+
+
+def margin_loss(y_true, y_pred):
+    """
+    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    :param y_true: [None, n_classes]
+    :param y_pred: [None, num_capsule]
+    :return: a scalar loss value.
+    """
+    # return tf.reduce_mean(tf.square(y_pred))
+    L = y_true * tf.square(tf.maximum(0., 0.9 - y_pred)) + \
+        0.5 * (1 - y_true) * tf.square(tf.maximum(0., y_pred - 0.1))
+
+    return tf.reduce_mean(tf.reduce_sum(L, 1))
+
+
+def train(model,  # type: models.Model
+          data, args):
+    """
+    Training a CapsuleNet
+    :param model: the CapsuleNet model
+    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    :param args: arguments
+    :return: The trained model
+    """
+    # unpacking the data
+    (x_train, y_train), (x_test, y_test) = data
+
+    # callbacks
+    log = callbacks.CSVLogger(args.save_dir + '/log.csv')
+    checkpoint = callbacks.ModelCheckpoint(args.save_dir + '/weights-{epoch:02d}.h5', monitor='val_capsnet_acc',
+                                           save_best_only=True, save_weights_only=True, verbose=1)
+    lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
+
+    # compile the model
+    model.compile(optimizer=optimizers.Adam(lr=args.lr),
+                  loss=[margin_loss, 'mse'],
+                  loss_weights=[1., args.lam_recon],
+                  metrics='accuracy')
+
+    """
+    # Training without data augmentation:
+    model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
+              validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint, lr_decay])
+    """
+
+    # Begin: Training with data augmentation ---------------------------------------------------------------------#
+    def train_generator(x, y, batch_size, shift_fraction=0.):
+        train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
+                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
+        generator = train_datagen.flow(x, y, batch_size=batch_size)
+        while 1:
+            x_batch, y_batch = generator.next()
+            yield (x_batch, y_batch), (y_batch, x_batch)
+
+    # Training with data augmentation. If shift_fraction=0., no augmentation.
+    model.fit(train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
+              steps_per_epoch=int(y_train.shape[0] / args.batch_size),
+              epochs=args.epochs,
+              validation_data=((x_test, y_test), (y_test, x_test)), batch_size=args.batch_size)
+    # End: Training with data augmentation -----------------------------------------------------------------------#
+
+    model.save_weights(args.save_dir + '/trained_model.h5')
+    print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
+
+    plot_log(args.save_dir + '/log.csv', show=True)
+
+    return model
+
+
+def test(model, data, args):
+    x_test, y_test = data
+    y_pred, x_recon = model.predict(x_test, batch_size=100)
+    print('-' * 30 + 'Begin: test' + '-' * 30)
+    print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1)) / y_test.shape[0])
+
+    img = combine_images(np.concatenate([x_test[:50], x_recon[:50]]))
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
+    print()
+    print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
+    print('-' * 30 + 'End: test' + '-' * 30)
+    plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
+    plt.show()
+
+
+def manipulate_latent(model, data, args):
+    print('-' * 30 + 'Begin: manipulate' + '-' * 30)
+    x_test, y_test = data
+    index = np.argmax(y_test, 1) == args.digit
+    number = np.random.randint(low=0, high=sum(index) - 1)
+    x, y = x_test[index][number], y_test[index][number]
+    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+    noise = np.zeros([1, 10, 16])
+    x_recons = []
+    for dim in range(16):
+        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+            tmp = np.copy(noise)
+            tmp[:, :, dim] = r
+            x_recon = model.predict([x, y, tmp])
+            x_recons.append(x_recon)
+
+    x_recons = np.concatenate(x_recons)
+
+    img = combine_images(x_recons, height=16)
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+    print('-' * 30 + 'End: manipulate' + '-' * 30)
+
+
+def load_mnist():
+    # the data, shuffled and split between train and test sets
+    from tensorflow.keras.datasets import mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
+    return (x_train, y_train), (x_test, y_test)
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import callbacks
+
+
+class Args:
+    def __init__(self):
+        self.epochs = 25
+        self.batch_size = 64
+        self.lr = 0.001
+        self.lr_decay = 0.9
+        self.lam_recon = 0.392
+        self.routings = 3
+        self.shift_fraction = 0.1
+        self.save_dir = '.'
+        self.digit = 5
+
+args = Args()
+
+# load data
+(x_train, y_train), (x_test, y_test) = load_mnist()
+# define model
+
+model, eval_model, manipulate_model = capsnets.CapsNet(input_shape=x_train.shape[1:],
+                                              n_class=len(np.unique(np.argmax(y_train, 1))),
+                                              routings=args.routings,
+                                              batch_size=args.batch_size)
+model.summary()
+
+train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
+test(model=eval_model, data=(x_test, y_test), args=args)
