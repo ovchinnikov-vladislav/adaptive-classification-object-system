e86e3f79eca2b82390068f5217a3bf9e66c85078
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-02-22 00:12:04 +0300

    удалены ненужные логи

--- bmstu/capsnet/layers/matrix.py
+++ bmstu/capsnet/layers/matrix.py
@@ -44,43 +44,24 @@ class ConvolutionalCapsule(tf.keras.Model, ABC):
         inputs_pose, inputs_activation = inputs
         batch_size = inputs_pose.shape[0]
         pose_size = inputs_pose.shape[-1]
-        print(f'pose_size: {pose_size}')
 
-        logg = f'inputs_pose: {inputs_pose.shape}'
         inputs_pose = kernel_tile(inputs_pose, 3, self.stride)
-        print(logg + f' -> {inputs_pose.shape}')
-
-        logg = f'inputs_activation: {inputs_activation.shape}'
         inputs_activation = kernel_tile(inputs_activation, 3, self.stride)
-        print(logg + f' -> {inputs_activation.shape}')
 
         spatial_size = int(inputs_activation.shape[1])
-        print(f'spatial_size: {spatial_size}')
-
-        logg = f'inputs_pose: {inputs_pose.shape}'
         inputs_pose = tf.reshape(inputs_pose, shape=[-1, 3 * 3 * self.i_size, 16])
-        print(logg + f' -> {inputs_pose.shape}')
-
-        logg = f'inputs_activation: {inputs_activation.shape}'
         inputs_activation = tf.reshape(inputs_activation, shape=[-1, spatial_size, spatial_size, 3 * 3 * self.i_size])
-        print(logg + f' -> {inputs_activation.shape}')
 
         votes = mat_transform(inputs_pose, self.o_size, size=batch_size * spatial_size * spatial_size)
-        logg = f'votes: {votes.shape}'
         votes = tf.reshape(votes, shape=[batch_size, spatial_size, spatial_size, votes.shape[-3], votes.shape[-2],
                                          votes.shape[-1]])
-        print(logg + f' -> {votes.shape}')
 
         glorot_uniform_initializer = tf.keras.initializers.GlorotUniform()
         beta_v = tf.Variable(lambda: glorot_uniform_initializer(shape=[1, 1, 1, self.o_size], dtype=tf.float32))
         beta_a = tf.Variable(lambda: glorot_uniform_initializer(shape=[1, 1, 1, self.o_size], dtype=tf.float32))
 
-        print(f'\nvotes: {votes.shape} inputs_activation: {inputs_activation.shape}')
         pose, activation = matrix_capsules_em_routing(votes, inputs_activation, beta_v, beta_a, self.routings)
-        print(f'pose: {pose.shape} activation: {activation.shape}')
-
         pose = tf.reshape(pose, shape=[pose.shape[0], pose.shape[1], pose.shape[2],
                                        pose.shape[3], pose_size, pose_size])
-        print(f'pose: {pose.shape} activation: {activation.shape}')
 
         return pose, activation
