9bd2193c4ecf3f1bdb5a66ac275517b5f45ca3cc
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-02-22 05:56:22 +0300

    рефакторинг

--- bmstu/capsnet/layers/vos.py
+++ bmstu/capsnet/layers/vos.py
@@ -1,12 +1,13 @@
-from abc import ABC
-
 import tensorflow as tf
 
 
-class PrimaryCapsule3D(tf.keras.Model, ABC):
+class PrimaryCapsule3D(tf.keras.Model):
     def __init__(self, capsules, dim_capsules, kernel_size, strides, name=''):
         super(PrimaryCapsule3D, self).__init__(name)
 
     def call(self, inputs, training=None, mask=None):
         # TODO: Написать первичные 3D капсулы
         pass
+
+    def get_config(self):
+        return super(PrimaryCapsule3D, self).get_config()
