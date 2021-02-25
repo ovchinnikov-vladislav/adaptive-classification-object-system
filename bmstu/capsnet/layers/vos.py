8456ee044a6fafe250255196e4c6b424e4091d07
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-02-25 23:52:34 +0300

    добавлен DenseCapsule слой

--- bmstu/capsnet/layers/vos.py
+++ bmstu/capsnet/layers/vos.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras import activations
 from functools import reduce
+from bmstu.capsnet.vos_utils import em_routing, create_coords_mat
 
 
 class PrimaryCapsule3D(tf.keras.layers.Layer):
@@ -27,6 +28,7 @@ class PrimaryCapsule3D(tf.keras.layers.Layer):
 
     def build(self, input_shape):
         self.batch_size = input_shape[0]
+        self.built = True
 
     def call(self, inputs, **kwargs):
         poses = self.conv3d_pose(inputs)
@@ -42,3 +44,79 @@ class PrimaryCapsule3D(tf.keras.layers.Layer):
 
     def get_config(self):
         return super(PrimaryCapsule3D, self).get_config()
+
+
+class DenseCapsule(tf.keras.layers.Layer):
+    """
+    :param capsules: The number of capsules in the following layer
+    :param route_min: A threshold activation to route
+    :param coord_add: A boolean, whether or not to to do coordinate addition
+    :param rel_center: A boolean, whether or not the coordinate addition is relative to the center
+    """
+
+    def __init__(self, capsules, dim_capsules, route_min=0.0,
+                 coord_add=False, rel_center=False, ch_same_w=True, **kwargs):
+        super(DenseCapsule, self).__init__(**kwargs)
+        self.dim_capsules = dim_capsules
+        self.dim_capsules_2 = dim_capsules * dim_capsules
+        self.batch_size = None
+        self.capsules = capsules
+        self.route_min = route_min
+        self.coord_add = coord_add
+        self.rel_center = rel_center
+        self.ch_same_w = ch_same_w
+        self.w = self.beta_v = self.beta_a = None
+        self.ch = None
+        self.n_capsch_i = None
+
+    def build(self, input_shape):
+        self.batch_size = input_shape[0][0]
+        shape_list = [int(x) for x in input_shape[0][1:]]
+        self.ch = int(shape_list[-2])
+        self.n_capsch_i = 1 if len(shape_list) == 2 else reduce((lambda x, y: x * y), shape_list[:-2])
+        if self.ch_same_w:
+            self.w = self.add_weight(shape=(self.ch, self.capsules, self.dim_capsules, self.dim_capsules),
+                                     initializer=tf.keras.initializers.random_normal(stddev=0.1),
+                                     regularizer=tf.keras.regularizers.L2(0.1))
+        else:
+            self.w = self.add_weight(shape=(self.n_capsch_i, self.ch, self.capsules,
+                                            self.dim_capsules, self.dim_capsules),
+                                     initializer=tf.keras.initializers.random_normal(stddev=0.1),
+                                     regularizer=tf.keras.regularizers.L2(0.1))
+
+        self.beta_v = self.add_weight(shape=(self.capsules, self.dim_capsules_2),
+                                      initializer=tf.keras.initializers.random_normal(stddev=0.1),
+                                      regularizer=tf.keras.regularizers.L2(0.1))
+        self.beta_a = self.add_weight(shape=(self.capsules, 1),
+                                      initializer=tf.keras.initializers.random_normal(stddev=0.1),
+                                      regularizer=tf.keras.regularizers.L2(0.1))
+
+        self.built = True
+
+    def call(self, inputs, **kwargs):
+        pose, activation = inputs
+
+        u_i = tf.reshape(pose, (self.batch_size, self.n_capsch_i, self.ch, self.dim_capsules_2))
+        activation = tf.reshape(activation, (self.batch_size, self.n_capsch_i, self.ch, 1))
+        coords = create_coords_mat(pose, self.rel_center) if self.coord_add else tf.zeros_like(u_i)
+
+        u_i = tf.reshape(u_i, (self.batch_size, self.n_capsch_i, self.ch, self.dim_capsules, self.dim_capsules))
+        u_i = tf.expand_dims(u_i, axis=-3)
+        u_i = tf.tile(u_i, [1, 1, 1, self.capsules, 1, 1])
+
+        votes = tf.einsum('ijab,ntijbc->ntijac', self.w, u_i)
+        votes = tf.reshape(votes, (self.batch_size, self.n_capsch_i * self.ch, self.capsules, self.dim_capsules_2))
+
+        if self.coord_add:
+            coords = tf.reshape(coords, (self.batch_size, self.n_capsch_i * self.ch, 1, self.dim_capsules_2))
+            votes = votes + tf.tile(coords, [1, 1, self.capsules, 1])
+
+        acts = tf.reshape(activation, (self.batch_size, self.n_capsch_i * self.ch, 1))
+        activations_result = tf.where(tf.greater_equal(acts, tf.constant(self.route_min)), acts, tf.zeros_like(acts))
+
+        capsules = em_routing(votes, activations_result, self.beta_v, self.beta_a)
+
+        return capsules
+
+    def get_config(self):
+        return super(DenseCapsule, self).get_config()
---
+++ bmstu/capsnet/vos_utils.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+
+epsilon = 1e-7
+
+
+def em_routing(v, a_i, beta_v, beta_a, n_iterations=3):
+    # TODO: VOS EM-routing
+    pass
+
+
+def create_coords_mat(pose, rel_center, dim_capsules):
+    # TODO: VOS Create-coords-mat
+    pass
