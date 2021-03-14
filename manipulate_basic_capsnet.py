3f48f811f55d17f1a510f2aedde2f155f6c25f34
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-03-14 19:34:29 +0300

    refactoring

---
+++ manipulate_basic_capsnet.py
@@ -0,0 +1,45 @@
+from bmstu import utls
+from bmstu.capsnet import models
+import numpy as np
+from PIL import Image
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--routings', default=3)
+parser.add_argument('--save_dir', default='./')
+parser.add_argument('--dataset', default='mnist', help='value: mnist, fashion_mnist, cifar10, cifar100')
+parser.add_argument('--digit', default=5)
+
+
+if __name__ == '__main__':
+    print('-' * 30 + 'Begin: manipulate' + '-' * 30)
+    args = parser.parse_args()
+
+    (x_train, y_train), (x_test, y_test) = utls.load(args.dataset)
+
+    _, _, model = models.CapsNet(shape=x_train.shape[1:],
+                                 num_classes=len(np.unique(np.argmax(y_train, 1))),
+                                 routings=args.routings).build()
+
+    model.load_weights(f'{args.save_dir}/manipulate_basic_capsnet_model_{args.dataset}.h5')
+
+    index = np.argmax(y_test, 1) == args.digit
+    number = np.random.randint(low=0, high=sum(index) - 1)
+    x, y = x_test[index][number], y_test[index][number]
+    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+    noise = np.zeros([1, 10, 16])
+    x_recons = []
+    for dim in range(16):
+        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+            tmp = np.copy(noise)
+            tmp[:, :, dim] = r
+            x_recon = model.predict([x, y, tmp])
+            x_recons.append(x_recon)
+
+    x_recons = np.concatenate(x_recons)
+
+    img = utls.combine_images(x_recons, height=16)
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+    print('-' * 30 + 'End: manipulate' + '-' * 30)
---
+++ test_basic_capsnet.py
@@ -0,0 +1,36 @@
+from bmstu import utls
+from bmstu.capsnet import models
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--routings', default=3)
+parser.add_argument('--save_dir', default='./')
+parser.add_argument('--dataset', default='mnist', help='value: mnist, fashion_mnist, cifar10, cifar100')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    (x_train, y_train), (x_test, y_test) = utls.load(args.dataset)
+
+    _, model, _ = models.CapsNet(shape=x_train.shape[1:],
+                                 num_classes=len(np.unique(np.argmax(y_train, 1))),
+                                 routings=args.routings).build()
+
+    model.load_weights(f'{args.save_dir}/eval_basic_capsnet_model_{args.dataset}.h5')
+
+    y_pred, x_recon = model.predict(x_test, batch_size=100)
+    print('-' * 30 + 'Begin: test' + '-' * 30)
+    print('y_test.shape[0]', y_test.shape[0])
+    print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1)) / y_test.shape[0])
+
+    img = utls.combine_images(np.concatenate([x_test[:50], x_recon[:50]]))
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
+    print()
+    print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
+    print('-' * 30 + 'End: test' + '-' * 30)
+    plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
+    plt.show()
