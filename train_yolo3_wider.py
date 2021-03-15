291c0401635c1f092e24344a9dcf5e815abdd926
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-03-15 16:02:58 +0300

    fix yolo

--- train_yolo3_wider.py
+++ train_yolo3_wider.py
@@ -3,23 +3,21 @@ from bmstu.datasets.wider_faces import wider_dataset_annotations
 from bmstu.yolo3.train import get_anchors, create_model, data_generator
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras.optimizers import Adam
-from bmstu.capsnets.losses import margin_loss
 
 import argparse
 
 parser = argparse.ArgumentParser(description='Train WIDER YOLO')
 parser.add_argument('--root_dataset', default='D:/tensorflow_datasets/',
                     help='path dataset ')
-parser.add_argument('--batch_size', default=10, type=int)
-parser.add_argument('--prepare_annotation', default=True, help='prepare annotation')
+parser.add_argument('--batch_size', default=5, type=int)
+parser.add_argument('--prepare_annotation', default=False, help='prepare annotation')
 parser.add_argument('--weights', default='model_data/yolo.dat', help='weights')
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
     root_path = args.root_dataset
     annotation_train_path, annotation_test_path, annotation_val_path = \
-        wider_dataset_annotations('D:/tensorflow_datasets/', is_prepare_annotation=True)
+        wider_dataset_annotations('D:/tensorflow_datasets/', is_prepare_annotation=args.prepare_annotation)
 
     anchors_path = 'model_data/yolo_anchors.txt'
     log_dir = 'logs/000/'
@@ -62,18 +60,15 @@ if __name__ == '__main__':
     #                         callbacks=[logging, checkpoint])
     #     model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
-    if True:
-        for i in range(len(model.layers)):
-            model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss=margin_loss)
-        print('Unfreeze all of the layers.')
-        batch_size = args.batch_size
-        print(f'Train on {num_train} samples, val on {num_val} samples, with batch size {batch_size}.')
-        model.fit_generator(generator=data_generator(train_lines, batch_size, input_shape, anchors, num_classes),
-                            steps_per_epoch=max(1, num_train // batch_size),
-                            validation_data=data_generator(val_lines, batch_size, input_shape, anchors, num_classes),
-                            validation_steps=max(1, num_val // batch_size),
-                            epochs=50,
-                            initial_epoch=0,
-                            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights(log_dir + 'trained_weights_final.h5')
+    model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred})
+    print('Unfreeze all of the layers.')
+    batch_size = args.batch_size
+    print(f'Train on {num_train} samples, val on {num_val} samples, with batch size {batch_size}.')
+    model.fit_generator(generator=data_generator(train_lines, batch_size, input_shape, anchors, num_classes),
+                        steps_per_epoch=max(1, num_train // batch_size),
+                        validation_data=data_generator(val_lines, batch_size, input_shape, anchors, num_classes),
+                        validation_steps=max(1, num_val // batch_size),
+                        epochs=50,
+                        initial_epoch=0,
+                        callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+    model.save_weights(log_dir + 'trained_weights_final.h5')
