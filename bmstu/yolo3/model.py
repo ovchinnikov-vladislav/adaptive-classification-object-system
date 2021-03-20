fe2155ff22320f8ffd8743683a11c7f912c7c096
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-03-20 22:41:50 +0300

    множественные фиксы

--- bmstu/yolo3/model.py
+++ bmstu/yolo3/model.py
@@ -2,38 +2,16 @@ import cv2
 import colorsys
 import tensorflow as tf
 import numpy as np
-from bmstu.yolo3.utils import transform_images, draw_outputs
+from bmstu.yolo3.utils import transform_images, analyze_outputs
 
 from bmstu.yolo3.layers import yolo_v3, yolo_v3_tiny
-
-
-class ObjectDetection:
-    def __init__(self, clazz, box, score):
-        self.clazz = clazz
-        self.box = box
-        self.score = score
-
-    def get_class(self):
-        return self.clazz
-
-    def get_box(self):
-        return self.box
-
-    def get_score(self):
-        return self.score
-
-    def __repr__(self):
-        return f'ObjectDetection[class = {self.clazz}, box = {self.box}, score = {self.score}]'
-
-    def __str__(self):
-        return f'ObjectDetection[class = {self.clazz}, box = {self.box}, score = {self.score}]'
 
 
 class YoloModel:
     def __init__(self, num_classes=80,
                  weights='./model_data/yolov3.tf',
                  classes='./model_data/coco_classes.txt',
-                 size=416):
+                 size=320):
         self.yolo = yolo_v3(classes=num_classes)
         self.yolo.make_predict_function()
         self.yolo.load_weights(weights).expect_partial()
@@ -52,17 +30,10 @@ class YoloModel:
         img = tf.expand_dims(image, 0)
         img = transform_images(img, self.size)
 
-        boxes, scores, classes, nums = self.yolo(img)
-
+        boxes, scores, classes, nums = self.yolo.predict(img)
         boxes, scores, classes, nums = boxes[0], scores[0], classes[0], nums[0]
-        object_detection = []
-        wh = np.flip(img.shape[0:2])
-        for i in range(nums):
-            x1, y1 = tuple((np.array(boxes[i][0:2]) * wh).astype(np.int32))
-            x2, y2 = tuple((np.array(boxes[i][2:4]) * wh).astype(np.int32))
-            object_detection.append(ObjectDetection(self.class_names[int(classes[i])], (x1, y1, x2, y2), scores[i]))
 
-        img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        img = draw_outputs(img, (boxes, scores, classes, nums), self.class_names, self.colors)
+        img, object_detection = analyze_outputs(np.array(image), (boxes, scores, classes, nums),
+                                                self.class_names, self.colors)
 
         return img, object_detection
--- videostreamflask.py
+++ videostreamflask.py
@@ -4,23 +4,38 @@ from bmstu.yolo3.model import YoloModel
 import numpy as np
 from flask import Flask, render_template, Response, make_response
 from urllib import request
+import time
+import streamlink
 
 
 class VideoCamera:
-    def __init__(self):
-        self.yolo = YoloModel()
-        # self.video = cv2.VideoCapture('http://192.168.0.16:8080/shot.jpg')
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(VideoCamera, cls).__new__(cls)
+            cls.instance.yolo = YoloModel()
+        return cls.instance
 
-    # def __del__(self):
-    #     self.video.release()
+    def set_video_path(self, video_id):
+        url = f'https://www.youtube.com/watch?v={video_id}'
+
+        streams = streamlink.streams(url)
+        self.video = cv2.VideoCapture(streams["720p"].url)
+        self.video.set(cv2.CAP_PROP_FPS, 24)
 
     def get_frame(self):
-        im = Image.open(request.urlopen('http://192.168.0.16:8080/shot.jpg'))
-        im = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-        image = self.yolo.detect_image(im)
-        result = np.asarray(image)
-        ret, jpeg = cv2.imencode('.jpg', result)
-        return jpeg.tobytes()
+        # img = Image.open(request.urlopen('http://192.168.0.16:8080/shot.jpg'))
+        return_value, img = self.video.read()
+        img = np.array(img)
+        t1 = time.time()
+        img, det_info = self.yolo.detect_image(img)
+        t2 = time.time()
+        fps = "FPS: " + str(int(1000 // int(1000 * (t2 - t1))))
+        cv2.putText(img, text=fps, org=(3, 15), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.50, color=(255, 0, 0), thickness=2)
+        img = np.asarray(img)
+        ret, img = cv2.imencode('.jpg', img)
+        # img = cv2.resize(img, (640, 480))
+        return img.tobytes()
 
 
 # @app.route('/shot.jpg')
@@ -31,20 +46,30 @@ class VideoCamera:
 #                      b'\r\n\r\n'), mimetype='multipart/x-mixed-replace;'
 #                                             'boundary=frame')
 
+camera = VideoCamera()
 
 if __name__ == '__main__':
     app = Flask(__name__, template_folder='templates/')
 
 
-    @app.route('/video')
-    def video():
+    @app.route('/shot.jpg')
+    def shot():
+        frame = camera.get_frame()
+        return Response((b'--frame\r\n'
+                         b'Content-Type: image/jpeg\r\n\r\n' + frame +
+                         b'\r\n\r\n'), mimetype='multipart/x-mixed-replace;'
+                                                'boundary=frame')
+
+    @app.route('/video/<video_id>')
+    def video(video_id):
+        camera.set_video_path(video_id)
         return render_template("index.html")
 
 
     @app.route('/video_feed')
     def video_feed():
-        return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace;'
-                                                     'boundary=frame')
+        return Response(gen(camera), mimetype='multipart/x-mixed-replace;'
+                                              'boundary=frame')
 
 
     def gen(cam):
@@ -54,5 +79,4 @@ if __name__ == '__main__':
                    b'Content-Type: image/jpeg\r\n\r\n' + frame +
                    b'\r\n\r\n')
 
-
-    app.run(host='localhost', port='5000', debug=True)
+    app.run(host='localhost', port='5000')
