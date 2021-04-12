f904e2cfd4a9bb8981ca76341a633d36bb9d0c8b
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-04-12 03:11:16 +0300

    fix 3level capsnet

--- train_3level_capsnet.py
+++ train_3level_capsnet.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
                   metrics='accuracy')
 
     model.fit(x_train, y_train, batch_size=args.batch_size, epochs=args.epochs,
-              validation_data=[x_test, y_test], callbacks=[log, tb, checkpoint, lr_decay])
+              validation_data=(x_test, y_test), callbacks=[log, tb, checkpoint, lr_decay])
 
     model.save_weights(f'{args.save_dir}/trained_basic_capsnet_model_{args.dataset}.h5')
 
