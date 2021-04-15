c8d22665a06b5494f66bbb6222a2598a7fba1fe0
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-04-15 19:29:21 +0300

    добавлены dropout слои для уменьшения эффекта переобученияЭ

--- libs/capsnets/models/rescaps.py
+++ libs/capsnets/models/rescaps.py
@@ -35,16 +35,16 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = Conv2D(filters1, (1, 1), kernel_regularizer=tf.keras.regularizers.l2, name=conv_name_base + '2a')(input_tensor)
+    x = Conv2D(filters1, (1, 1), kernel_regularizer=tf.keras.regularizers.l2(0.001), name=conv_name_base + '2a')(input_tensor)
     x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters2, kernel_size, kernel_regularizer=tf.keras.regularizers.l2,
+    x = Conv2D(filters2, kernel_size,
                padding='same', name=conv_name_base + '2b')(x)
     x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c', kernel_regularizer=tf.keras.regularizers.l2)(x)
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c', kernel_regularizer=tf.keras.regularizers.l2(0.001))(x)
     x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
 
     x = Dropout(0.5)(x)
