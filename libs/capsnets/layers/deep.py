8181712af1898fb7fd0953ed509ecf0d22e6397b
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-04-18 19:16:18 +0300

    добавлен слой FlattenCapsule

--- libs/capsnets/layers/deep.py
+++ libs/capsnets/layers/deep.py
@@ -27,3 +27,23 @@ class ConvertToCapsule(layers.Layer):
 
     def get_config(self):
         return super(ConvertToCapsule, self).get_config()
+
+
+class FlattenCapsule(layers.Layer):
+    def __init__(self, **kwargs):
+        super(FlattenCapsule, self).__init__(**kwargs)
+        self.input_spec = layers.InputSpec(min_ndim=4)
+
+    def call(self, inputs, **kwargs):
+        shape = inputs.shape
+        return tf.reshape(inputs, shape=(-1, np.prod(shape[1:-1]), shape[-1]))
+
+    def compute_output_shape(self, input_shape):
+        if not all(input_shape[1:]):
+            raise ValueError('The shape of the input to "FlattenCaps" '
+                             'is not fully defined '
+                             '(got ' + str(input_shape[1:]) + '. '
+                             'Make sure to pass a complete "input_shape" '
+                             'or "batch_input_shape" argument to the first '
+                             'layer in your model.')
+        return input_shape[0], np.prod(input_shape[1:-1]), input_shape[-1]
