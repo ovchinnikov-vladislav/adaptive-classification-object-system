cda1ff748a74f9f6de567704002be204eb247f56
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-05-05 19:27:25 +0300

    fix

--- ui/src/main/java/bmstu/dynamic/simulator/controller/MainController.java
+++ ui/src/main/java/bmstu/dynamic/simulator/controller/MainController.java
@@ -24,6 +24,69 @@ public class MainController {
         return "index";
     }
 
+    @GetMapping("/blank")
+    public String blank(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "blank";
+    }
+
+    @GetMapping("/buttons")
+    public String buttons(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "buttons";
+    }
+
+    @GetMapping("/cards")
+    public String cards(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "cards";
+    }
+
+    @GetMapping("/charts")
+    public String charts(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "charts";
+    }
+
+    @GetMapping("/tables")
+    public String tables(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "tables";
+    }
+
+    @GetMapping("/utilities-animation")
+    public String utilitiesAnimation(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "utilities-animation";
+    }
+
+    @GetMapping("/utilities-border")
+    public String utilitiesBorder(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "utilities-border";
+    }
+
+    @GetMapping("/utilities-color")
+    public String utilitiesColor(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "utilities-color";
+    }
+
+    @GetMapping("/utilities-other")
+    public String utilitiesOther(Model model) {
+        modelService.prepareStandardModelForTemplate(model);
+
+        return "utilities-other";
+    }
+
     @GetMapping("/error/{status}")
     public String error(Model model, @PathVariable Integer status) {
         modelService.prepareErrorModelForTemplate(model, status);
--- ui/src/main/java/bmstu/dynamic/simulator/service/ModelService.java
+++ ui/src/main/java/bmstu/dynamic/simulator/service/ModelService.java
@@ -11,26 +11,24 @@ public class ModelService {
 
     public void prepareStandardModelForTemplate(Model model) {
         model.addAttribute("now_year", LocalDate.now().getYear());
-        model.addAttribute("copyright", "Vladislav Ovchinnikov");
+        model.addAttribute("copyright", "Владислав Овчинников");
     }
 
     public void prepareErrorModelForTemplate(Model model, Integer status) {
-        model.addAttribute("status", "Undefined");
+        model.addAttribute("status", "Неопределенная ошибка");
 
         if (HttpStatus.valueOf(status) == HttpStatus.NOT_FOUND) {
             model.addAttribute("status", status);
-            model.addAttribute("text", "Page Not Found");
-            model.addAttribute("additional_text",  "It looks like you found a glitch in the matrix...");
+            model.addAttribute("text", "Страница или ресурс не найдены");
         } else if (HttpStatus.valueOf(status) == HttpStatus.BAD_REQUEST) {
             model.addAttribute("status", status);
-            model.addAttribute("text", "Page Not Found");
-            model.addAttribute("additional_text",  "It looks like you found a glitch in the matrix...");
+            model.addAttribute("text", "Запрос недействителен");
         } else if (HttpStatus.valueOf(status).is5xxServerError()) {
             model.addAttribute("status", status);
-            model.addAttribute("text", "Fatal error");
+            model.addAttribute("text", "Фатальная ошибка (ошибка сервера)");
         }
+        model.addAttribute("additional_text",  "Похоже вы нашли баг в матрице...");
 
-        model.addAttribute("now_year", LocalDate.now().getYear());
-        model.addAttribute("copyright", "Vladislav Ovchinnikov");
+        prepareStandardModelForTemplate(model);
     }
 }
