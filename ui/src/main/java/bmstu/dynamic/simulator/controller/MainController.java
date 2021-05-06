6adc148f1de8e9feed73b8323796ce85f62dbacb
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-05-06 04:42:14 +0300

    fix all

--- ui/src/main/java/bmstu/dynamic/simulator/controller/MainController.java
+++ ui/src/main/java/bmstu/dynamic/simulator/controller/MainController.java
@@ -2,22 +2,15 @@ package bmstu.dynamic.simulator.controller;
 
 import bmstu.dynamic.simulator.service.ModelService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
 @Controller
 @RequestMapping("/")
@@ -36,100 +29,40 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/blank")
-    public String blank(Model model,
-                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                        @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
+    @GetMapping("/profile")
+    public String profile(Model model,
+                          @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                          @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("user_id", oauth2User.getName());
         model.addAttribute("user_attributes", oauth2User.getAttributes());
         model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "blank";
+        return "profile";
     }
 
-    @GetMapping("/buttons")
-    public String buttons(Model model,
-                @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                @AuthenticationPrincipal OAuth2User oauth2User) {
-            model.addAttribute("user_name", oauth2User.getName());
-            model.addAttribute("user_attributes", oauth2User.getAttributes());
-            model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "buttons";
-    }
-
-    @GetMapping("/cards")
-    public String cards(Model model,
-                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                        @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
+    @GetMapping("/settings")
+    public String settings(Model model,
+                           @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                           @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("user_id", oauth2User.getName());
         model.addAttribute("user_attributes", oauth2User.getAttributes());
         model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "cards";
+        return "settings";
     }
 
-    @GetMapping("/charts")
-    public String charts(Model model,
-                         @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                         @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
+    @GetMapping("/activity")
+    public String activity(Model model,
+                           @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                           @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("user_id", oauth2User.getName());
         model.addAttribute("user_attributes", oauth2User.getAttributes());
         model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "charts";
-    }
-
-    @GetMapping("/tables")
-    public String tables(Model model,
-                         @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                         @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
-        model.addAttribute("user_attributes", oauth2User.getAttributes());
-        model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "tables";
-    }
-
-    @GetMapping("/utilities-animation")
-    public String utilitiesAnimation(Model model,
-                                     @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                                     @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
-        model.addAttribute("user_attributes", oauth2User.getAttributes());
-        model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "utilities-animation";
-    }
-
-    @GetMapping("/utilities-border")
-    public String utilitiesBorder(Model model,
-                                  @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                                  @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
-        model.addAttribute("user_attributes", oauth2User.getAttributes());
-        model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "utilities-border";
-    }
-
-    @GetMapping("/utilities-color")
-    public String utilitiesColor(Model model,
-                                 @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                                 @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
-        model.addAttribute("user_attributes", oauth2User.getAttributes());
-        model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "utilities-color";
-    }
-
-    @GetMapping("/utilities-other")
-    public String utilitiesOther(Model model,
-                                 @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                                 @AuthenticationPrincipal OAuth2User oauth2User) {
-        model.addAttribute("user_name", oauth2User.getName());
-        model.addAttribute("user_attributes", oauth2User.getAttributes());
-        model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
-        return "utilities-other";
+        return "activity";
     }
 
     @GetMapping("/lk_error/{status}")
     public String lkError(@PathVariable Integer status, Model model,
-                        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-                        @AuthenticationPrincipal OAuth2User oauth2User) {
+                          @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                          @AuthenticationPrincipal OAuth2User oauth2User) {
         model.addAttribute("user_name", oauth2User.getName());
         model.addAttribute("user_attributes", oauth2User.getAttributes());
         model.addAttribute("client_name", authorizedClient.getClientRegistration().getClientName());
