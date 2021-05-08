3c9d2d8e6c764f26137732280c3ca99635cd7ab4
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-05-08 17:49:21 +0300

    undo fix

--- algorithms/config.py
+++ algorithms/config.py
@@ -6,7 +6,7 @@ yolo_v4_weights = os.path.abspath('./model_data/yolov4.tf')
 caps_yolo_weights = os.path.abspath('...')
 
 yolo_anchors = os.path.abspath('./model_data/yolo_anchors.txt')
-coco_classes_ru = os.path.abspath('./model_data/coco_classes_ru.txt')
+coco_classes_ru = os.path.abspath('./model_data/coco_classes_en.txt')
 coco_classes_en = os.path.abspath('./model_data/coco_classes_en.txt')
 
 deepsort_model = os.path.abspath('./model_data/deepsort.pb')
