4e4ee4e788f60bd181e5b3b289ae90932e4cba29
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-06-11 03:00:45 +0300

    fix parse tfrecord

--- algorithms/libs/detection/utils.py
+++ algorithms/libs/detection/utils.py
@@ -409,7 +409,7 @@ IMAGE_FEATURE_MAP = {
 }
 
 
-def parse_tfrecord(tfrecord, class_table, size, yolo_max_boxes=30):
+def parse_tfrecord(tfrecord, class_table, size, yolo_max_boxes=100):
     x = tf.io.parse_single_example(tfrecord, IMAGE_FEATURE_MAP)
     x_train = tf.image.decode_jpeg(x['image/encoded'], channels=3)
     x_train = tf.image.resize(x_train, (size, size))
