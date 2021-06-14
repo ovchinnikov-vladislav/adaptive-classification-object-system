5d393ba2e9b1cbb955c67bc78bda31901121deed
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2021-06-14 04:00:38 +0300

    fixes

--- algorithms/api/classification_api.py
+++ algorithms/api/classification_api.py
@@ -9,8 +9,4 @@ classification_api = Blueprint('classification_api', __name__)
 def video_classification():
     file = request.files['file']
     npzfile = np.load(file)
-    print(npzfile['arr_0'].shape)
-    # arr = np.fromstring(data['video'], dtype=float).reshape((8, 112, 112, 3))
-    # print(arr.shape)
-    #
-    # return classification_video(arr)
+    return classification_video(npzfile['arr_0'])
